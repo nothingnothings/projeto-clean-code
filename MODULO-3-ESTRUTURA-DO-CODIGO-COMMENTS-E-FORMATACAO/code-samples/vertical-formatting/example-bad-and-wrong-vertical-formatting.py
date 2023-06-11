01a817fb778a9edefbaaf499b8ca933a92c6e533
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-06-11 13:09:35 -0300

    finished lesson 6

---
+++ MODULO-3-ESTRUTURA-DO-CODIGO-COMMENTS-E-FORMATACAO/code-samples/vertical-formatting/example-bad-and-wrong-vertical-formatting.py
@@ -0,0 +1,54 @@
+
+# start()
+
+
+
+# def start():
+#     print('Start')
+#     next()
+
+
+
+# # WONT WORK 
+# # start()
+
+
+
+# def next():
+#     print('next')
+#     last()
+
+
+
+# def last():
+#     print('last')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#WILL WORK:
+
+
+def start():
+    print('Start')
+    next()
+
+def next():
+    print('next')
+    last()
+
+def last():
+    print('last')
+
+start()
